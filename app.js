bd50a4e87198ab8be0b532a71f1c2f9efd4de0f1
unknown <souravprajapati31@gmail.com>
2021-01-02 21:01:04 +0530

    database

--- app.js
+++ app.js
@@ -4,21 +4,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose= require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+mongoose.connect("mongodb://localhost:27017/Blog",{ useNewUrlParser: true } );
 const app = express();
-const contentUp=[];
+
 //use
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
+//models
+const post={
+  title:String,
+  body:String
+};
+const postMode= mongoose.model("post",post);
 //Request
 app.get('/',(req,resp)=>{
-   resp.render("home",{'startContent':homeStartingContent,post:contentUp});
+   const contentUp=[];
+   postMode.find(function(error,data){
+     for(var i=0;i<data.length;i++)
+     {
+      const temp={
+        title:data[i].title,
+        body:data[i].body
+      };
+      contentUp.push(temp);
+    }
+    console.log(contentUp);
+    this.contentUp=contentUp;
+    resp.render("home",{'startContent':homeStartingContent,post:contentUp});
+   });
+
 });
 app.get('/about',(req,resp)=>{
   resp.render('about',{'startContent':aboutContent});
@@ -42,14 +63,21 @@ app.get('/home/:itemTitle',(req,resp)=>{
       console.log("Fail");
   });
 });
-//Post request Handel
 
+
+//Post request Handel
 app.post('/postCompose',(req,resp)=>{
   var data={
     title : _.lowerCase(req.body.title),
     body : req.body.body
   };
-  contentUp.push(data);
+  var opost=new postMode(
+    {
+      title : _.lowerCase(req.body.title),
+      body : req.body.body
+    }
+  );
+  opost.save();
   resp.redirect('/');
 });
 app.listen(3000, function() {
